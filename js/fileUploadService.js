db8bd06b6b3e145c8899163896a008681869a02e
Dev-sungmin <sungminmail0@naver.com>
2025-11-13 14:17:49 +0900

    fix: fileUploadService.js

--- js/fileUploadService.js
+++ js/fileUploadService.js
@@ -3,7 +3,7 @@ window.FileUploadService = class {
     this.apiService = window.apiService;
     
     // API Gateway 엔드포인트 (나중에 실제 URL로 교체)
-    this.apiGatewayUrl = 'https://TEMP-API-GATEWAY-URL/presign';
+    this.apiGatewayUrl = 'https://j9cutt34d2.execute-api.ap-northeast-2.amazonaws.com/presign';
     
     console.log("FileUpload - API Gateway URL:", this.apiGatewayUrl);
   }
